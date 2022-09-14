98b77ae1af405588a2c298a92079cc79b4ce9595
Michael Flückiger <contact@michaelflueckiger.ch>
2022-09-14 19:12:41 +0200

    Basis

---
+++ Basis/sketch.js
@@ -0,0 +1,46 @@
+let numRows = 3;
+let numCols = 3;
+let tileSize = 200;
+let patternarray = [];
+
+function preload() {
+  patternarray.push(loadImage("assets/0.png"));
+  patternarray.push(loadImage("assets/1.png"));
+  patternarray.push(loadImage("assets/2.png"));
+  patternarray.push(loadImage("assets/3.png"));
+  patternarray.push(loadImage("assets/4.png"));
+  patternarray.push(loadImage("assets/5.png"));
+}
+
+function setup() {
+  createCanvas(numRows * tileSize, numCols * tileSize);
+  frameRate(60);
+  noLoop();
+}
+
+function draw() {
+  background(220, 0, 0);
+
+  for (var y = 0; y < numCols; y++) {
+    for (var x = 0; x < numRows; x++) {
+      push();
+      translate(x * tileSize, y * tileSize);
+
+      //   noStroke();
+      //  fill(random(255), random(255), random(255));
+      //  rect(0, 0, tileSize, tileSize);
+
+      image(random(patternarray), 0, 0, tileSize, tileSize);
+      pop();
+    }
+  }
+}
+
+function keyPressed() {
+  if (key == "r") {
+    redraw();
+  }
+  if (key == "s") {
+    saveCanvas("myPattern", ".png");
+  }
+}
