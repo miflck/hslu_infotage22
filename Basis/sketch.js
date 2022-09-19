45e8f1cb2b22272fa7af4c5dbb1a018cdb4345ea
Michael Flückiger <contact@michaelflueckiger.ch>
2022-09-19 17:15:08 +0200

    subdivide

--- Basis/sketch.js
+++ Basis/sketch.js
@@ -1,7 +1,8 @@
 let numRows = 3;
 let numCols = 3;
-let tileSize = 200;
+let tileSize = 400;
 let patternarray = [];
+let subdivide = 4;
 
 function preload() {
   patternarray.push(loadImage("assets/0.png"));
@@ -10,6 +11,10 @@ function preload() {
   patternarray.push(loadImage("assets/3.png"));
   patternarray.push(loadImage("assets/4.png"));
   patternarray.push(loadImage("assets/5.png"));
+  patternarray.push(loadImage("assets/dreieck 1.png"));
+  patternarray.push(loadImage("assets/dreieck 2.png"));
+  patternarray.push(loadImage("assets/dreieck 3.png"));
+  patternarray.push(loadImage("assets/dreieck 4.png"));
 }
 
 function setup() {
@@ -19,16 +24,32 @@ function setup() {
 }
 
 function draw() {
-  background(220, 0, 0);
+  background(255);
   for (var y = 0; y < numCols; y++) {
     for (var x = 0; x < numRows; x++) {
       console.log("y", y, "x", x);
       push();
       translate(x * tileSize, y * tileSize);
-      //   noStroke();
-      //  fill(random(255), random(255), random(255));
-      //  rect(0, 0, tileSize, tileSize);
-      image(random(patternarray), 0, 0, tileSize, tileSize);
+      subdivide = int(random(2, 4));
+      if (int(random(0, 2)) > 0) {
+        for (var y1 = 0; y1 < subdivide; y1++) {
+          for (var x1 = 0; x1 < subdivide; x1++) {
+            push();
+            translate((x1 * tileSize) / subdivide, (y1 * tileSize) / subdivide);
+            image(
+              random(patternarray),
+              0,
+              0,
+              tileSize / subdivide,
+              tileSize / subdivide
+            );
+            stroke(255, 0, 0);
+            pop();
+          }
+        }
+      } else {
+        image(random(patternarray), 0, 0, tileSize, tileSize);
+      }
       pop();
     }
   }
