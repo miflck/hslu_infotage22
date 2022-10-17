df9c68fc5234abed44f338cd626bc9a0558d027d
Michael Flückiger <contact@michaelflueckiger.ch>
2022-10-17 09:53:17 +0200

    upload

--- Basis/sketch.js
+++ Basis/sketch.js
@@ -1,16 +1,11 @@
-let numRows = 3;
-let numCols = 3;
-let tileSize = 200;
 let patternarray = [];
 
 let visWidth = 600;
 let visHeight = 600;
 
-let imageArray = [];
 let paramArray = [];
 
-let sel;
-let sel2;
+let canvas;
 
 function preload() {
   patternarray.push(loadImage("assets/0.png"));
@@ -19,15 +14,16 @@ function preload() {
   patternarray.push(loadImage("assets/3.png"));
   patternarray.push(loadImage("assets/4.png"));
   patternarray.push(loadImage("assets/5.png"));
-  patternarray.push(loadImage("assets/dreieck 1.png"));
-  patternarray.push(loadImage("assets/dreieck 2.png"));
-  patternarray.push(loadImage("assets/dreieck 3.png"));
-  patternarray.push(loadImage("assets/dreieck 4.png"));
+  patternarray.push(loadImage("assets/6.png"));
+  patternarray.push(loadImage("assets/7.png"));
+  patternarray.push(loadImage("assets/8.png"));
+  patternarray.push(loadImage("assets/9.png"));
 }
 
 function setup() {
-  createCanvas(numRows * tileSize, numCols * tileSize);
-  colorMode(HSB, 360, 100, 100);
+  canvas = createCanvas(visWidth, visHeight);
+
+  colorMode(HSB, 360, 100, 100, 100);
   frameRate(60);
   noLoop();
 }
@@ -35,36 +31,20 @@ function setup() {
 function draw() {
   background(255);
 
-  /*for (var y = 0; y < numCols; y++) {
-    for (var x = 0; x < numRows; x++) {
-      //console.log("y", y, "x", x);
-      push();
-      translate(x * tileSize, y * tileSize);
-      //   noStroke();
-      //  fill(random(255), random(255), random(255));
-      //  rect(0, 0, tileSize, tileSize);
-      image(random(patternarray), 0, 0, tileSize, tileSize);
-      pop();
-    }
-  }*/
-
   paramArray.forEach((element, index) => {
-    console.log(element);
     push();
-    // translate((index % numRows) * tileSize, floor(index / numCols) * tileSize);
     translate(0, 0);
+
+    // farben festlegen. das kann natürlich z.b auch an ein dropdown geknüpft werden
     let hue = map(index, 0, paramArray.length, 0, 360);
     let saturation = 80;
     let brightness = 80;
 
-    console.log("hue", hue);
-
+    //blendMode(DIFFERENCE);
+    // das png wird eingefärbt: achtung, es muss weiss auf transparent sein
     tint(hue, saturation, brightness);
-
-    //fill(255, 0, 0);
-    rect(0, 0, tileSize, tileSize);
+    // rect(0, 0, tileSize, tileSize);
     image(patternarray[element], 0, 0, visWidth, visHeight);
-
     pop();
   });
 }
@@ -74,25 +54,54 @@ function keyPressed() {
     redraw();
   }
   if (key == "s") {
-    saveCanvas("myPattern", ".png");
+    //saveCanvas("myPattern", ".png");
+
+    var c = canvas.canvas.toDataURL("image/png");
+    console.log("c", c);
+    //asyncCall();
+    console.log("h");
+
+    const e = document.createElement("div");
+    e.innerHTML = "JavaScript DOM";
+    document.body.appendChild(e);
+
+    //asyncCall();
+
+    var dataURL = canvas.canvas.toDataURL();
+    //console.log(dataURL);
+
+    const img = document.createElement("img");
+    img.width = width;
+    img.height = height;
+    img.src = dataURL;
+    document.body.appendChild(img);
+
+    /*let response = fetch("./upload/upload.php", {
+      // HTTP request type
+      method: "POST",
+      // Sending our blob with our request
+      body: { imgBase64: c },
+      image: c,
+    });*/
+
+    fetch("./upload/upload.php", {
+      method: "POST",
+      mode: "no-cors",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: c,
+    })
+      .then((response) => response.text())
+      .then((success) => console.log(success))
+      .catch((error) => console.log(error));
   }
 }
 
+// immer wenn ein dropdown bedient wird, wird diese funktion ausgelöst:
+// der value des dropdowns wird ausgelesen und das entsprechende bild in den paramArray geladen
 function getValue(selectObject) {
   var value = parseInt(selectObject.value);
   var id = parseInt(selectObject.id);
   paramArray[id] = value;
   console.log(id, value, paramArray);
-
   redraw();
-}
-
-function changeBg() {
-  console.log(this.value(), this.attribute("thing"), this.id());
-  let val = sel.value();
-  /*if (val == "Red") {
-    redraw();
-  } else if (val == "Blue") {
-  } else if (val == "Green") {
-  }*/
 }
---
+++ Basis/upload/upload.php
@@ -0,0 +1,111 @@
+<?php
+
+$img = file_get_contents("php://input"); // $_POST didn't work
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$data = base64_decode($img);
+
+if (!file_exists($_SERVER['DOCUMENT_ROOT'] . "/img")) {
+    mkdir($_SERVER['DOCUMENT_ROOT'] . "/img", 0777, true);
+}
+
+$file = $_SERVER['DOCUMENT_ROOT'] . "/img/".time().'.png';
+
+$success = file_put_contents($file, $data);
+print $success ? $file.' saved.' : 'Unable to save the file.';
+
+  /*  $data = $_POST['photo'];
+    list($type, $data) = explode(';', $data);
+    list(, $data)      = explode(',', $data);
+    $data = base64_decode($data);
+    mkdir($_SERVER['DOCUMENT_ROOT'] . "/photos");
+    file_put_contents($_SERVER['DOCUMENT_ROOT'] . "/photos/".time().'.png', $data);
+    die;
+*/
+//define('UPLOAD_DIR', '/dings');
+
+
+/*define('UPLOAD_DIR', 'images/');
+mkdir($_SERVER['DOCUMENT_ROOT'] . "/dings");
+$data = $_POST['imgBase64'];
+$data = str_replace('data:image/png;base64,', '', $data);
+$data = base64_decode( str_replace(' ', '+', $data) );
+$file =UPLOAD_DIR. uniqid() . '.png';
+//$success = file_put_contents($file, $data);
+//print $success ? $file : 'Unable to save the file.';
+$success = file_put_contents($_SERVER['DOCUMENT_ROOT'] .'/dings/'.uniqid().'.png', $data);
+$success = file_put_contents($file, $data);
+*/
+
+//mkdir($_SERVER['DOCUMENT_ROOT'] . "/images");
+/*
+define('UPLOAD_DIR', 'images/');
+$img = $_POST['imgBase64'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$data = base64_decode($img);
+$file = UPLOAD_DIR . uniqid() . '.png';
+$success = file_put_contents($file, $data);
+//send request to ocr /
+	print $success ? $file : 'Unable to save the file.';
+//mkdir($_SERVER['DOCUMENT_ROOT'] . "/dings");
+/*
+define('UPLOAD_DIR', $_SERVER['DOCUMENT_ROOT'] .'images/');
+	$img = $_POST['img'];
+	$img = str_replace('data:image/png;base64,', '', $img);
+	$img = str_replace(' ', '+', $img);
+	$data = base64_decode($img);
+	$file = UPLOAD_DIR . uniqid() . '.png';
+	$success = file_put_contents($file, $data);
+	print $success ? $file : 'Unable to save the file.';
+*/
+    /*
+move_uploaded_file(
+    $_FILES['file']['tmp_name'], 
+    $_SERVER['DOCUMENT_ROOT'] . "/dings/test.png"
+); 
+die;
+$target_dir = "dings/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+*/
+?>
